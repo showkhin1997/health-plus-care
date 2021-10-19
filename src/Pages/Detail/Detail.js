8049b68c9e74a06fc399d8966039874fc333be6a
showkhin1997 <showkhin15@gmail.com>
2021-10-19 23:32:29 +0600

    service details update

--- src/Pages/Detail/Detail.js
+++ src/Pages/Detail/Detail.js
@@ -24,20 +24,22 @@ const Detail = () => {
 
 
     return (
-        <div classNam="card h-100 mt-5 mb-5">
-            <img src={singleSeviceDetail?.img} className="card-img-top single-service-img" alt="..." />
-            <div className="card-body">
-                <h3 className="card-title fs-1">{singleSeviceDetail?.name}</h3>
-                <h6 className="text-success">Doctor: {singleSeviceDetail?.doctorName}</h6>
-                <p className="text-success">Designation: {singleSeviceDetail?.doctorType}</p>
-                <p className="card-text">{singleSeviceDetail?.description}</p>
-                <p className="text-muted">Visiting Time: {singleSeviceDetail?.timeStart}AM - {singleSeviceDetail?.timeEnd}PM</p>
-                <p className="text-muted">Contuct number: {singleSeviceDetail?.contuctNumber}</p>
-                <h5 className="text-primary">Cost: ${singleSeviceDetail?.visitAmmount}</h5>
-                <button type="button" className="btn btn-primary mt-5 mb-5">Appoinment</button>
-                <Nav.Link as={HashLink} to="/home#home">
-                    <button type="button" className="btn btn-primary">Back to Home</button>
-                </Nav.Link>
+        <div className="w-100 p-5">
+            <div classNam="card h-100 mt-5 mb-5">
+                <img src={singleSeviceDetail?.img} className="card-img-top w-75 h-50" alt="..." />
+                <div className="card-body">
+                    <h3 className="card-title fs-1">{singleSeviceDetail?.name}</h3>
+                    <h6 className="text-success">Doctor: {singleSeviceDetail?.doctorName}</h6>
+                    <p className="text-success">Designation: {singleSeviceDetail?.doctorType}</p>
+                    <p className="card-text w-75">{singleSeviceDetail?.description}</p>
+                    <p className="text-muted">Visiting Time: {singleSeviceDetail?.timeStart}AM - {singleSeviceDetail?.timeEnd}PM</p>
+                    <p className="text-muted">Contuct number: {singleSeviceDetail?.contuctNumber}</p>
+                    <h5 className="text-primary">Visiting Cost: ${singleSeviceDetail?.visitAmmount}</h5>
+                    <button type="button" className="btn btn-primary mt-5 mb-5">Appoinment</button>
+                    <Nav.Link as={HashLink} to="/home#home">
+                        <button type="button" className="btn btn-primary">Back to Home</button>
+                    </Nav.Link>
+                </div>
             </div>
         </div>
     );
