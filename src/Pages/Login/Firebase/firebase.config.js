f276386026103225dfaeb82152becefb56a7e36f
showkhin1997 <showkhin15@gmail.com>
2021-10-20 13:44:30 +0600

    enviornment variable added

--- src/Pages/Login/Firebase/firebase.config.js
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyA6a1D2k9zlv17D9YIWk3xGk5KFCGlSlKE",
-    authDomain: "health-plus-70731.firebaseapp.com",
-    projectId: "health-plus-70731",
-    storageBucket: "health-plus-70731.appspot.com",
-    messagingSenderId: "1250752843",
-    appId: "1:1250752843:web:1e59953c862c442b9590ce"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 export default firebaseConfig;
